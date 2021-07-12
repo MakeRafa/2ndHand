7e42dbdecaa826d0aecb5e837e58419921c285f3
MakeRafa <rafavazquez229@gmail.com>
2021-07-11 21:53:41 -0700

    added required libraries

---
+++ server.js
@@ -0,0 +1,32 @@
+require('dotenv').config()
+
+var express = require('express');
+var exphbs  = require('express-handlebars');
+
+const cookieParser = require('cookie-parser');
+
+const checkAuth = require('./middleware/checkAuth');
+//APP SETUP
+var app = express();
+
+
+app.engine('handlebars', exphbs());
+app.set('view engine', 'handlebars');
+
+// Add this after you initialize express.
+app.use(cookieParser());
+
+app.use(checkAuth);
+
+app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
+
+app.use(express.static('public'));
+
+
+module.exports = app;
+
+// Start Server
+app.listen(3000, () => {
+    console.log('Reddit Clone listening on port localhost:3000!');
+});
