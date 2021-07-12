8d44c8ca3c2d6e48ebbbb7eaa33ab58ba4dc372e
MakeRafa <rafavazquez229@gmail.com>
2021-07-12 10:38:03 -0700

    test commit

--- routes/index.js
+++ routes/index.js
@@ -1,12 +1,9 @@
 var express = require('express');
 var router = express.Router();
-
-
-
 // home 
 
 router.get('/', function(req, res, next) {
-    res.render('index.hbs', { title: '2ndHand' });
+    res.render('home', { title: '2ndHand' });
 });
 
 module.exports = router;
--- server.js
+++ server.js
@@ -1,36 +1,12 @@
-// require('dotenv').config()
-// const hbs = require('express-handlebars');
 
 var express = require('express');
 var exphbs  = require('express-handlebars');
 
-// const cookieParser = require('cookie-parser');
-
 //APP SETUP
 var app = express();
 
-
-// app.engine('hbs', exphbs());
-// app.set('view engine', 'hbs');
-
-// // app.engine('hbs', hbs({
-// //     layoutsDir: __dirname + '/views',
-// //     // partialsDir: path.join(__dirname, 'views/partials'),
-// //     defaultLayout: 'index',
-// //     extname: 'hbs'
-// // }));
-// // app.set('views', path.join(__dirname, 'views'));
-// // app.set('view engine', 'hbs');
-// // Add this after you initialize express.
-// // app.use(cookieParser());
-
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
-
-// app.use(express.static('public'));
-
-
-// module.exports = app;
 
 // Start Server
 app.listen(3000, () => {
