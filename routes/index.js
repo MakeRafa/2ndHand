2caabffcdffebc5218626647bd38784867ea08c8
Sammmmmuel <69543188+Sammmmmuel@users.noreply.github.com>
2021-07-12 09:02:28 -0700

    fixed layout for working habdlebars, also got server running

--- routes/index.js
+++ routes/index.js
@@ -2,10 +2,11 @@ var express = require('express');
 var router = express.Router();
 
 
+
 // home 
 
 router.get('/', function(req, res, next) {
-    res.render('index', { title: '2ndHand' });
+    res.render('index.hbs', { title: '2ndHand' });
 });
 
 module.exports = router;
--- server.js
+++ server.js
@@ -1,32 +1,63 @@
-require('dotenv').config()
+// require('dotenv').config()
+// const hbs = require('express-handlebars');
 
-var express = require('express');
-var exphbs = require('express-handlebars');
+// var express = require('express');
+// var exphbs = require('express-handlebars');
 
-const cookieParser = require('cookie-parser');
+// // const cookieParser = require('cookie-parser');
 
-const checkAuth = require('./middleware/checkAuth');
-//APP SETUP
-var app = express();
-
-
-app.engine('handlebars', exphbs());
-app.set('view engine', 'handlebars');
-
-// Add this after you initialize express.
-// app.use(cookieParser());
-
-app.use(checkAuth);
-
-app.use(express.json());
-app.use(express.urlencoded({ extended: false }));
-
-app.use(express.static('public'));
+// // const checkAuth = require('./middleware/checkAuth');
+// //APP SETUP
+// var app = express();
 
 
-module.exports = app;
+// app.engine('hbs', exphbs());
+// app.set('view engine', 'hbs');
 
-// Start Server
-app.listen(3000, () => {
-    console.log('2ndHand listening on port localhost:3000!');
+// // app.engine('hbs', hbs({
+// //     layoutsDir: __dirname + '/views',
+// //     // partialsDir: path.join(__dirname, 'views/partials'),
+// //     defaultLayout: 'index',
+// //     extname: 'hbs'
+// // }));
+// // app.set('views', path.join(__dirname, 'views'));
+// // app.set('view engine', 'hbs');
+// // Add this after you initialize express.
+// // app.use(cookieParser());
+
+// // app.use(checkAuth);
+
+// app.use(express.json());
+// app.use(express.urlencoded({ extended: false }));
+
+// app.use(express.static('public'));
+
+
+// module.exports = app;
+
+// // Start Server
+// app.listen(3000, () => {
+//     console.log('2ndHand listening on port localhost:3000!');
+// });
+
+const express = require('express')
+const app = express()
+const port = 3000
+    // const hbs = require('express-handlebars');
+const exphbs = require('express-handlebars');
+
+app.engine('hbs', exphbs({
+    defaultLayout: 'main',
+    extname: '.hbs'
+}));
+
+app.set('view engine', 'hbs');
+
+app.get('/', (req, res) => {
+    res.render('home');
 });
+
+
+app.listen(port, () => {
+    console.log(`2ndHand listening at http://localhost:${port}`)
+})
