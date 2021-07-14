b82406ef938b9a7b4dd56b9d1fbce448625c9ce5
Sammmmmuel <69543188+Sammmmmuel@users.noreply.github.com>
2021-07-13 19:58:33 -0700

    added posts page and routes for it.

--- routes/posts.js
+++ routes/posts.js
@@ -1,4 +1,5 @@
 var express = require('express');
+const { post } = require('../server');
 // home 
 
 module.exports = (app) => {
@@ -9,4 +10,12 @@ module.exports = (app) => {
     app.get('/', (req, res) => {
         res.render('home');
     });
+
+    app.get('/posts-page', (req, res) => {
+        post.find({}).lean()
+            .then((posts) => res.render('posts-index', { posts }))
+            .catch((err) => {
+                console.log(err.message);
+            })
+    })
 };
