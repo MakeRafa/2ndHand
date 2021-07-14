2b13aaa1ec6aa93025aa6154973abc5d4f065c38
Sammmmmuel <69543188+Sammmmmuel@users.noreply.github.com>
2021-07-13 22:03:33 -0700

    posts show

--- models/post.js
+++ models/post.js
@@ -1,23 +1,13 @@
 const { Schema, model } = require('mongoose');
 
 const postSchema = new Schema({
-<<<<<<< HEAD
     title: { type: String, required: true },
-    //     picture: { 
-    //         file: {type: Buffer, required: true},
-    //         filename: {type: String, required: true},  
+    // picture: {
+    //     file: { type: Buffer, required: true },
+    //     filename: { type: String, required: true },
     // },
-    //     size: { type: String, required: true},
-    //     price: { type: String, required: true},
-=======
-    title: { type: String, required: true},
-//     picture: { 
-//         file: {type: Buffer, required: true},
-//         filename: {type: String, required: true},  
-// },
-//     size: { type: String, required: true},
-//     price: { type: String, required: true},
->>>>>>> 01fce7e5e86eb96b10dbc2ac5c4a203fdfc12931
+    size: { type: String, required: true },
+    price: { type: String, required: true },
 
 })
 
--- routes/posts.js
+++ routes/posts.js
@@ -4,14 +4,7 @@ const Post = require('../models/post');
 // home 
 
 module.exports = (app) => {
-<<<<<<< HEAD
-    // CREATE
-=======
-    // // CREATE
->>>>>>> 01fce7e5e86eb96b10dbc2ac5c4a203fdfc12931
-    // app.get('/', (req, res) => {
-    //     res.render('home');
-    // })
+
 
     app.get('/posts/new', (req, res) => {
         res.render('posts-new');
@@ -23,11 +16,7 @@ module.exports = (app) => {
     })
 
     app.get('/', (req, res) => {
-<<<<<<< HEAD
-        post.find({}).lean()
-=======
         Post.find({}).lean()
->>>>>>> 01fce7e5e86eb96b10dbc2ac5c4a203fdfc12931
             .then((posts) => res.render('posts-index', { posts }))
             .catch((err) => {
                 console.log(err.message);
