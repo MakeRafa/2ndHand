3bdfe92d71da01ee5407681803d85728f2fb4fd6
MakeRafa <rafavazquez229@gmail.com>
2021-07-13 19:58:34 -0700

    added schema model for posts

--- routes/posts.js
+++ routes/posts.js
@@ -1,12 +1,19 @@
 var express = require('express');
+const Post = require('../models/post');
 // home 
 
 module.exports = (app) => {
     // CREATE
-    app.get('/posts/new', (req, res) => {
-        res.render('posts-new');
-    })
     app.get('/', (req, res) => {
         res.render('home');
     });
+
+    app.get('/posts/new', (req, res) => {
+        res.render('posts-new');
+    })
+    app.post('/posts/new', (req, res) =>{
+        const post = new Post(req.body);
+
+        post.save(() => res.redirect('/post-index'))
+    })
 };
