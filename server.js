6bfbc8bf6348130794ff6f303943681532bd44d9
MakeRafa <rafavazquez229@gmail.com>
2021-07-13 19:29:53 -0700

    added db

--- server.js
+++ server.js
@@ -1,45 +1,3 @@
-// require('dotenv').config()
-// const hbs = require('express-handlebars');
-
-// var express = require('express');
-// var exphbs = require('express-handlebars');
-
-// // const cookieParser = require('cookie-parser');
-
-// // const checkAuth = require('./middleware/checkAuth');
-// //APP SETUP
-// var app = express();
-
-
-// app.engine('hbs', exphbs());
-// app.set('view engine', 'hbs');
-
-// // app.engine('hbs', hbs({
-// //     layoutsDir: __dirname + '/views',
-// //     // partialsDir: path.join(__dirname, 'views/partials'),
-// //     defaultLayout: 'index',
-// //     extname: 'hbs'
-// // }));
-// // app.set('views', path.join(__dirname, 'views'));
-// // app.set('view engine', 'hbs');
-// // Add this after you initialize express.
-// // app.use(cookieParser());
-
-// // app.use(checkAuth);
-
-// app.use(express.json());
-// app.use(express.urlencoded({ extended: false }));
-
-// app.use(express.static('public'));
-
-
-// module.exports = app;
-
-// // Start Server
-// app.listen(3000, () => {
-//     console.log('2ndHand listening on port localhost:3000!');
-// });
-
 const express = require('express')
 const app = express()
 const port = 3000
@@ -61,6 +19,7 @@ app.get('/posts/new', (req, res) => {
     res.render('posts-new');
 })
 
+require('./data/secondHand-db');
 
 app.listen(port, () => {
     console.log(`2ndHand listening at http://localhost:${port}`)
